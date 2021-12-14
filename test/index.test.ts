b710960ba35b8ca0c15e19c027dc9d8d6e916fe6
monodyle <monodylh@gmail.com>
2021-12-14 21:00:57 +0700

    test: update test cases

--- test/index.test.ts
+++ test/index.test.ts
@@ -4,25 +4,61 @@ import rehypeRaw from "rehype-raw";
 import remark2rehype from "remark-rehype";
 import remarkParse from "remark-parse";
 import stringify from "rehype-stringify";
-import rehypeCustomEmoji from "../src";
+import rehypeCustomEmoji, { RehypeEmojiOptions } from "../src";
 
-const mrkStr = "# Hello\n\n```:hello:```\n\nWhat a beautiful day :hello:";
 const emojis = {
   hello: "/path/hello.png",
+  wave: "https://example.com/wave.gif",
 };
 
+const parser = (content: string, options: RehypeEmojiOptions) =>
+  unified()
+    .use(remarkParse)
+    .use(remark2rehype, { allowDangerousHtml: true })
+    .use(rehypeRaw)
+    .use(rehypeCustomEmoji, options)
+    .use(stringify)
+    .processSync(content)
+    .toString();
+
 describe("rehypeCustomEmoji", () => {
-  it("findAndReplace", () => {
+  it("<p> tag", () => {
+    const mrkStr = "# Hello\n\n```:hello:```\n\nWhat a beautiful day :hello:";
     const expected =
-      '<h1>Hello</h1>\n\n<p><code>:hello:</code></p>\n\n<p>What a beautiful day <img src="/path/hello.png" class="emoji"></p>';
-    const htmlStr = unified()
-      .use(remarkParse)
-      .use(remark2rehype, { allowDangerousHtml: true })
-      .use(rehypeRaw)
-      .use(rehypeCustomEmoji, { emojis })
-      .use(stringify)
-      .processSync(mrkStr)
-      .toString();
+      '<h1>Hello</h1>\n<p><code>:hello:</code></p>\n<p>What a beautiful day <img src="/path/hello.png" class="emoji"></p>';
+    const htmlStr = parser(mrkStr, { emojis });
+    expect(htmlStr).toEqual(expected);
+  });
+
+  it("[data-emoji] property", () => {
+    const mrkStr = "# Hello\n\n<div>Hello <span data-emoji>:wave:</span></div>";
+    const expected =
+      '<h1>Hello</h1>\n<div>Hello <span data-emoji=""><img src="https://example.com/wave.gif" class="emoji"></span></div>';
+    const htmlStr = parser(mrkStr, { emojis });
+    expect(htmlStr).toEqual(expected);
+  });
+
+  it("custom className", () => {
+    const mrkStr = "Hello, I'm rehypeCustomEmoji :wave:";
+    const expected =
+      '<p>Hello, I\'m rehypeCustomEmoji <img src="https://example.com/wave.gif" class="customEmoji"></p>';
+    const htmlStr = parser(mrkStr, { emojis, className: "customEmoji" });
+    expect(htmlStr).toEqual(expected);
+  })
+
+  it("ignore <div>", () => {
+    const mrkStr = "# Hello\n\n<div>Hello :wave:</div>";
+    const expected =
+      '<h1>Hello</h1>\n<div>Hello :wave:</div>';
+    const htmlStr = parser(mrkStr, { emojis, ignore: "div" });
+    expect(htmlStr).toEqual(expected);
+  });
+
+  it("ignore <div> and <p>", () => {
+    const mrkStr = "# Hello\n\n<div>Hello :wave:</div>\n\n:hello:";
+    const expected =
+      '<h1>Hello</h1>\n<div>Hello :wave:</div>\n<p>:hello:</p>';
+    const htmlStr = parser(mrkStr, { emojis, ignore: ["div", "p"] });
     expect(htmlStr).toEqual(expected);
   });
 });
